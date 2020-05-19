eb06c630a3fba0a3cd191b6af5444242a72d1af5
Balbader <15971200+Balbader@users.noreply.github.com>
2020-05-19 13:44:17 +0200

    Update III_5.c

--- C_III/III_5.c
+++ C_III/III_5.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-	int n;
-	int p;
+	int	n;
+	int	p;
 
 	printf("Enter value for n and p followed by 'Enter': ");
 	scanf("%d %d", &n, &p);
+	
 	printf("n value is: %d\np value is: %d\n", n, p);
 
 	return(0);
