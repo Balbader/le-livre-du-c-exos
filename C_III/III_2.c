5d81ce849cda208380ccd507f2897da5c8ad46db
Balbader <15971200+Balbader@users.noreply.github.com>
2020-05-19 13:42:07 +0200

    Update III_2.c

--- C_III/III_2.c
+++ C_III/III_2.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	float tva;
-	int nombre_article;
-	float prix_unitaire;
-	float prix_ht;
-	float prix_ttc;
+	int	nombre_article;
+	float	tva;
+	float	prix_unitaire;
+	float	prix_ht;
+	float	prix_ttc;
 	
 	tva = 19.6;
 	nombre_article = 5;
 	prix_unitaire = 8.43;
+	
 	prix_ht = prix_unitaire * nombre_article;
 	prix_ttc = (((prix_ht / 100) * tva) + prix_ht) * nombre_article;
 	printf("nombre d article\t:%6d\nprix HT\t\t\t:%9.2f\nprix TTC\t\t:%9.2f\n", nombre_article, prix_ht, prix_ttc);
