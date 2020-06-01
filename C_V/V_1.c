73fb20d2bf991b71994d1ed3752f428ca3750673
Balbader <15971200+Balbader@users.noreply.github.com>
2020-06-01 17:00:16 +0200

    Update V_1.c

--- C_V/V_1.c
+++ C_V/V_1.c
@@ -9,7 +9,7 @@ int main(void)
 		printf("Donnez un entier positif inférieur a 100 : ");
 		scanf("%d", &nbr_entier);
 	}
-	while (nbr_entier <= 0 || nbr_entier >= 100)
+	while (nbr_entier <= 0 || nbr_entier >= 100);
 		printf("Merci pour le nombre : %d\n", nbr_entier);
 
 	return 0;
