83652863d041fcc47b0e15bb1eac0761f7cb0207
Balbader <15971200+Balbader@users.noreply.github.com>
2020-06-15 07:48:28 +0200

    Update VII_1.c

--- C_VII/VII_1.c
+++ C_VII/VII_1.c
@@ -12,20 +12,6 @@ int main(void)
 
     for (k = 0; k < 6; k++)
         printf("%d ", val[k]);
-    
-    printf("\n\n");
-
-    int suite[8];
-    int i;
-
-    suite[0] = 1;
-    suite[1] = 1;
-    
-    for (i = 2; i < 8; i++)
-        suite[i] = suite[i - 1] + suite[i - 2];
-    
-    for (i = 0; i < 8; i++)
-        printf("%d\n", suite[i]);
 
     return 0;
 }
