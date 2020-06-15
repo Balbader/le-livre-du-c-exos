d7d5cc8422de62c5588895e223c289aa2e3bf315
Balbader <15971200+Balbader@users.noreply.github.com>
2020-06-15 07:53:15 +0200

    Update VI_1.c

--- C_VI/VI_1.c
+++ C_VI/VI_1.c
@@ -1,14 +1,11 @@
-/*  Ecrire un programme qui lit 20 notes entières
-    et qui indiques le pourcentage de notes supérieures à 10 */
-
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    int i;          /*pour le compteur de boucle sur les 5 notes*/
-    int n_moy;      /*compteur du nombre de notes supérieures à 10*/
-    int note;       /*pour lire une note à évaluer*/
-    float pourcent; /*pourcentage de notes supérieures à 10*/
+    int     i;
+    int     n_moy;
+    int     note;
+    float   pourcent;
 
     n_moy = 0;
 
@@ -16,9 +13,9 @@ int main()
 
     for (i = 1; i <= 5; i++)
     {
-        scanf("%d", &note); /*lecture d'une note*/
+        scanf("%d", &note);
         if (note > 10)
-            n_moy++; /* +1 sur le compteur si supérieure à 10*/
+            n_moy++;
     }
     if (n_moy == 0)
         printf("aucune note supérieure à 10.\n");
@@ -27,5 +24,5 @@ int main()
         pourcent = (100.0 * n_moy) / 5;
         printf("il y'a %.f%% pourcent de notes supérieures à 10.\n", pourcent);
     }
-    return (0);
+    return 0;
 }
