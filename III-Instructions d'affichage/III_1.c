abdfb931ec5e12a45e4c702b61e7d1195c995130
Balbader <15971200+Balbader@users.noreply.github.com>
2020-06-27 18:31:17 +0200

    exos chap 3

---
+++ III-Instructions d'affichage/III_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	char cat;
+	char art;
+	int qte;
+
+	cat = 'B';
+	art = 'S';
+	qte = 50;
+	printf("%d articles %c, de catégorie %c\n", qte, art, cat);
+
+	return(0);
+}
