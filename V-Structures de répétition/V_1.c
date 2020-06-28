32f4fd1de13e42928a5f1b073265a0f96cf809c8
Balbader <15971200+Balbader@users.noreply.github.com>
2020-06-28 17:10:08 +0200

    exos chap 5

---
+++ V-Structures de répétition/V_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int		nbr_entier;
+	
+	do
+	{
+		printf("Donnez un entier positif inférieur a 100 : ");
+		scanf("%d", &nbr_entier);
+	}
+	while (nbr_entier <= 0 || nbr_entier >= 100);
+		printf("Merci pour le nombre : %d\n", nbr_entier);
+
+	return(0);
+}
