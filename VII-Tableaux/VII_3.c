44e377ef466bd42576003093975837df9468b6e9
Balbader <15971200+Balbader@users.noreply.github.com>
2020-06-29 19:03:52 +0200

    exos chap 7

---
+++ VII-Tableaux/VII_3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define N 5
+
+int main(void)
+{
+    int     t[N] = {1, 30, 120, 20, 40};
+    int     max; //Highest element of the array
+    int     pos; //Position of the highest element
+    int     i;
+    
+    pos = 0;
+    max = t[0];
+
+    for (i = 0; i < N; i++)
+    {
+        if (t[i] > max)
+            max = t[i];
+        
+        if (t[i] > t[pos])
+            pos = i;
+    }
+    
+    printf("Highest #: %d\n", max);
+    printf("Postion: %d\n", pos);   
+    
+    return 0;
+}
+
