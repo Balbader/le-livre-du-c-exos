4fa4d81764f1ebdbaa73de4b280cc0467943124c
Balbader <15971200+Balbader@users.noreply.github.com>
2020-07-02 21:15:05 +0200

    Update IV_3.c

--- IV-if/IV_3.c
+++ IV-if/IV_3.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	int a;
-	int b;
-	int c;
+    int a;
+    int b;
+    int c;
 
-	printf("Merci d'entrer un premier nombre entier de votre choix: ");
-	scanf("%d", &a);
+    printf("Merci d'entrer un premier nombre entier de votre choix: ");
+    scanf("%d", &a);
 	
-	printf("Merci d'entrer un deuxieme nombre entier de votre choix: ");
-	scanf("%d", &b);
+    printf("Merci d'entrer un deuxieme nombre entier de votre choix: ");
+    scanf("%d", &b);
 	
-	printf("Merci d'entrer un troisieme nombre entier de votre choix: ");
-	scanf("%d", &c);
+    printf("Merci d'entrer un troisieme nombre entier de votre choix: ");
+    scanf("%d", &c);
 
-	if ((a < b) && (b < c))
-		printf("\nAffichage des données entrés par ordre decroissant:\na = %d\tb = %d\tc = %d", a, b, c);
-	else
-		printf("\nAffichage des données entrés:\na = %d\tb = %d\tc = %d", a, b, c);
-
-	return(0);
+    if ((a < b) && (b < c))
+    {
+	printf("\nAffichage des données entrés par ordre decroissant:\na = %d\tb = %d\tc = %d", a, b, c);
+    } 	    
+    else
+    {
+	printf("\nAffichage des données entrés:\na = %d\tb = %d\tc = %d", a, b, c);
+    }
+    return(0);
 }
