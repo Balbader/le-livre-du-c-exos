24bf4adc5d5a23d9a6b867761251be0310e94b71
Balbader <15971200+Balbader@users.noreply.github.com>
2020-07-02 21:19:16 +0200

    Update IV_6.c

--- IV-if/IV_6.c
+++ IV-if/IV_6.c
@@ -2,31 +2,32 @@
 
 int main(void)
 {
-	float	montant;
-	float	mont_net;
-	float	remise;
+    float    montant;
+    float    mont_net;
+    float    remise;
 	
-	printf("Merci d'entrer le prix de l'article desiré (minimum de 2000eur) : ");
-	scanf("%f", &montant);
+    printf("Merci d'entrer le prix de l'article desiré (minimum de 2000eur) : ");
+    scanf("%f", &montant);
 
-	if (montant >= 2000 && montant <= 5000)
-	{
-		remise = montant * 1 / 100;
-		mont_net = montant - remise;
+    if (montant >= 2000 && montant <= 5000)
+    {
+	remise = montant * 1 / 100;
+	mont_net = montant - remise;
 		
-		printf("Remise : %.2f\n", remise);
-		printf("Le montant net avec la remise est de : %.2f\n", mont_net);
-	}
-	else if (montant > 5000)
-	{
-		remise = montant * 2 / 100;
-		mont_net = montant - remise;
+	printf("Remise : %.2f\n", remise);
+	printf("Le montant net avec la remise est de : %.2f\n", mont_net);
+    }
+    else if (montant > 5000)
+    {
+	remise = montant * 2 / 100;
+	mont_net = montant - remise;
 		
-		printf("Remise : %.2f\n", remise);
-		printf("Le montant net avec la remise est de : %.2f\n", mont_net);
-	}
-	else
-		printf("Pas de remise possible.\n");
-
-	return(0);
+	printf("Remise : %.2f\n", remise);
+	printf("Le montant net avec la remise est de : %.2f\n", mont_net);
+    }
+    else
+    {
+	printf("Pas de remise possible.\n");
+    }
+    return(0);
 }
