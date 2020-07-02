98b1c005aa0d002dda7cf216a570603762dc0b10
Balbader <15971200+Balbader@users.noreply.github.com>
2020-07-02 21:17:35 +0200

    Update IV_5.c

--- IV-if/IV_5.c
+++ IV-if/IV_5.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	float	a;
-	float	b;
-	float	racine;
+    float    a;
+    float    b;
+    float    racine;
 
-	printf("Donnez les coefficients: ");
-	scanf("%e %e", &a, &b);
+    printf("Donnez les coefficients: ");
+    scanf("%e %e", &a, &b);
 
-	if (a != 0)
-	{
-		racine = -b / a;
-		printf("solution: %e\n", racine);
-	}
-	else if (b == 0)
-		printf("Solution indéterminée\n");
-	else
-		printf("Pas de solution\n");
-
-	return(0);
+    if (a != 0)
+    {
+	racine = -b / a;
+	printf("solution: %e\n", racine);
+    }
+    else if (b == 0)
+    {
+	printf("Solution indéterminée\n");
+    }
+    else
+    {
+	printf("Pas de solution\n");
+    }
+    return(0);
 }
