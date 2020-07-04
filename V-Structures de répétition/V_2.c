b61ee2c7f9a863844e465fd9783950091327e234
Balbader <15971200+Balbader@users.noreply.github.com>
2020-07-04 17:38:13 +0200

    Update V_2.c

--- V-Structures de répétition/V_2.c
+++ V-Structures de répétition/V_2.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
  
-int main()
+int main(void)
 {
-	int     nbr_entier;   
+    int     nbr_entier;   
 		
-	printf("Donnez un entier positif inférieur a 100 : ");
+    printf("Donnez un entier positif inférieur a 100 : ");
 	
-	do
-	{
-		scanf("%d", &nbr_entier);
-		if (nbr_entier <= 0 || nbr_entier >= 100)
-			printf("SVP positif inférieur a 100 : ");
-	}
-	while (nbr_entier <= 0 || nbr_entier >= 100);
-	printf("Merci pour le nombre : %d\n", nbr_entier);
+    do
+    {
+	scanf("%d", &nbr_entier);
+	if (nbr_entier <= 0 || nbr_entier >= 100)
+		printf("SVP positif inférieur a 100 : ");
+    }
+    while (nbr_entier <= 0 || nbr_entier >= 100);
+    printf("Merci pour le nombre : %d\n", nbr_entier);
 	
-	return(0);
+    return(0);
 }
