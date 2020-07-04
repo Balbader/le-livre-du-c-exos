54b4353024adb44fb3faefe4601178c7cc5499cc
Balbader <15971200+Balbader@users.noreply.github.com>
2020-07-04 17:44:05 +0200

    Update V_4.c

--- V-Structures de répétition/V_4.c
+++ V-Structures de répétition/V_4.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	float	cap_ini;
-	float	cap;
-	float	taux;
-	int		i;
+    float    cap_ini;
+    float    cap;
+    float    taux;
+    int      i;
 
-	i = 0;
+    i = 0;
 
-	printf("donnez le capital a placer et le taux : ");
-	scanf("%e%e", &cap_ini, &taux);
-	cap = cap_ini;
+    printf("donnez le capital a placer et le taux : ");
+    scanf("%e%e", &cap_ini, &taux);
+    cap = cap_ini;
 
-	do
-	{
-		i++;
-		cap = cap * (1 + taux);
-		printf("capital, a l'année %d : %12.2f\n", i,  cap);
-	}
-	while (cap <= 2 * cap_ini);
+    do
+    {
+        i++;
+	cap = cap * (1 + taux);
+	printf("capital, a l'année %d : %12.2f\n", i,  cap);
+    }
+    while (cap <= 2 * cap_ini);
 
-	return(0);
+    return (0);
 }
